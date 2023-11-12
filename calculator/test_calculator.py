78d4ed1fa15879848fa9f9fefc21ce5c823fac4d
agwugloria <gloriaawgu@ymail.com>
2023-11-11 22:11:37 -0600

    adding calculator & requirement files

---
+++ calculator/test_calculator.py
@@ -0,0 +1,17 @@
+import calculator
+
+class TestCalculator: 
+
+def test_addition (self):
+    assert 4 == calculator.add(2,2)
+
+def test_addition (self)
+    assert 3 == calculator.add (2,2)
+
+def test_subtraction (self):
+    assert 2 == calculator.subtract(4,2)
+
+def test_miltiplication (self):
+    assert 100 == calculator.multiply(10,10)
+
+
